ce0f4bb927bf792f446d598ba81267accf2031af
Mohit koley <mohitkoley5@gmail.com>
2025-06-08 15:59:43 +0530

    refactor: migrate from CommonJS to ES modules
    - update package.json to use "type": "module"
    - convert require statements to import syntax
    - add new User model and repository files

---
+++ second_project/src/models/user/user.js
@@ -0,0 +1,11 @@
+export class User{
+    constructor(id, name, email, password, role = "user"){
+       this.id = id;
+       this.name = name;
+       this.email = email;
+       this.password = password;
+       this.role = role;
+       this.createdAt = new Date();   
+    }
+}
+
---
+++ second_project/src/repositories/user/user_repository.js
@@ -0,0 +1,8 @@
+import { User } from '../../models/user/user.js';
+
+var users = [
+    new User(1, "user1", "abc@gmail.com", "12345"),
+];
+
+console.log(users);
+
